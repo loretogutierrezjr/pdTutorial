8d9f1e87792a0d0fc6f0b6062e07687762775053
Loreto Gutierrez <loretogutierrezjr@users.noreply.github.com>
2017-09-22 00:24:51 +0300

    Add files via upload

---
+++ var_primitive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var_primitive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberTen = 10;
+            string firstName = "Peter";
+            char middleInitial = 'B';
+            double height = 63.5;
+            decimal depositamount = 1270.50m;
+            bool isAssigned = true;
+
+            Console.WriteLine(numberTen);
+            Console.WriteLine(firstName);
+            Console.WriteLine(middleInitial);
+            Console.WriteLine(height);
+            Console.WriteLine(depositamount);
+            Console.WriteLine(isAssigned);
+            Console.ReadKey();
+        }
+    }
+}
