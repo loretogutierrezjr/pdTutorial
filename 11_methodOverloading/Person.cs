e9200ad63daeeb2c994e2a8b64d1dbf5e5277851
Helpdesk BaaN <helpdesk@rc-xch.com>
2017-09-29 15:51:30 +0300

    added method overloading

---
+++ 11_methodOverloading/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_methodOverloading
+{
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+
+        }
+
+        public string SayHello(string name)
+        {
+            return "Hello there, " + name;
+        }
+
+        public string SayHello(Person person)
+        {
+            return "Hello there, " + 
+                person.FirstName + " " + person.LastName;
+        }
+    }
+}
---
+++ 11_methodOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_methodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jane", "Doe");
+            Console.WriteLine(person.SayHello("Peter Smith"));
+
+            Person friend = new Person("Chuck", "Norris");
+            Console.WriteLine(person.SayHello(friend));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
