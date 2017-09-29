8b6b695b829907fa151f7db3dce942282fa34bba
Helpdesk BaaN <helpdesk@rc-xch.com>
2017-09-29 14:16:29 +0300

    added person class

---
+++ 08_personClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_personClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+        }
+    }
+}
