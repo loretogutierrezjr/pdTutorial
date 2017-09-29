68649c2f9f06d9e2f13b617d0329eef009067e83
Helpdesk BaaN <helpdesk@rc-xch.com>
2017-09-29 15:32:40 +0300

    added constructors

---
+++ 09_constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("john", "doe");
+            Console.WriteLine(person.SayHello("jerem"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
