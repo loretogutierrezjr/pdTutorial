2abe4c171445c1f092a5c51c8a77e3c7171c8415
loretogutierrezjr <loretogutierrezjr@hotmail.com>
2017-10-08 16:12:54 +0300

    add new project option parameters

---
+++ 12_OptionParameters/Mail.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_OptionParameters
+{
+    class Mail
+    {
+        public StringBuilder MailMessage { get; private set; }
+
+        public Mail()
+        {
+            
+        }
+
+        public void SendMail ( 
+            
+            string toAddress,
+            string bodyText,
+            bool ccAdministrator = true,
+            bool isBodyHtml = false )
+        
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("To: " + toAddress);
+
+            if (ccAdministrator)
+            {
+                sb.AppendLine("Cc: admin@xyz.com");
+            }
+
+            if (isBodyHtml)
+            {
+                sb.AppendLine("HTML: " + bodyText);
+
+            }
+
+            else
+            {
+                sb.AppendLine("Message: " + bodyText);
+
+            }
+
+            this.MailMessage = sb;
+
+        }
+    }
+}
---
+++ 12_OptionParameters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_OptionParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mail mail = new Mail();
+            
+            mail.SendMail("pd@abc.com", "Please read this email.");
+            Console.WriteLine(mail.MailMessage);
+
+            mail.SendMail("fe@acme.com", "Acknowledge receipt.", false, true);
+            Console.WriteLine(mail.MailMessage);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
