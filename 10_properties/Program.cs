639ddff0ff8a9af2c9294effd09c6cf44ebdfb8f
loretogutierrezjr <loretogutierrezjr@hotmail.com>
2017-10-08 14:57:15 +0300

    another test comment

--- 10_properties/Program.cs
+++ 10_properties/Program.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace _10_properties
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            myClassProp.Person person = new myClassProp.Person("John", "Doe");
-            person.FirstName = "Xavier";
-            Console.WriteLine(person.FirstName);
-            Console.WriteLine(person.LastName); 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			myClassProp.Person person = new myClassProp.Person("John", "Doe");
+			person.FirstName = "Xavier";
+			Console.WriteLine(person.FirstName);
+			Console.WriteLine(person.LastName); 
 
-            myClassProp.Person name = new myClassProp.Person("Loreto", "Gutierrez");
-            name.FirstName = "Retz";
-            Console.WriteLine(name.FirstName);
-            Console.WriteLine(name.LastName);
+			myClassProp.Person name = new myClassProp.Person("Loreto", "Gutierrez");
+			name.FirstName = "Retz";
+			Console.WriteLine(name.FirstName);
+			Console.WriteLine(name.LastName);
 
-            Console.WriteLine(name);
+			Console.WriteLine(name);
 
-            //this a readkey
-	    //another comment from helpdesk
-            Console.ReadKey();
+			//this a readkey
+			//another comment from helpdesk
+			Console.ReadKey();
 
-        }
-    }
+		}
+	}
 }
