18519b6c8b971c2a2b9e3da2787aa81ad1bd5cc3
loretogutierrezjr <loretogutierrezjr@hotmail.com>
2017-10-30 15:27:13 +0300

    conditions

---
+++ 13_conditions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = 6 >= 5;
+            Console.WriteLine(result);
+
+            //-------------
+
+            bool staticMethods = string.IsNullOrEmpty("Test");
+            Console.WriteLine(staticMethods);
+
+            //-------------
+
+            Person loreto = new Person("Loreto");
+            bool logicalAnd = loreto != null && (loreto.Name == loreto.Name);
+            Console.WriteLine(logicalAnd);
+
+            //-------------
+
+
+
+
+            //-------------
+            Console.ReadKey();
+        }
+    }
+}
