fe932364899138141c0be1296bc2d9bddc8ae004
loretogutierrezjr <loretogutierrezjr@hotmail.com>
2017-10-30 16:19:37 +0300

    validating parameters

---
+++ 14_validatingParameters/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_validatingParameters
+{
+    class Person
+    {
+        public string Name { get; private set; }
+
+        public Person(string name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name cannot be null, empty, or whitespace");
+            }
+
+            this.Name = name;
+        }
+
+        public string SayHello(string name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name cannot be null, empty, or whitespace");
+            }
+
+            return "Hello there, " + name;
+        }
+
+        public string SayHello(Person person)
+        {
+            if (person == null)
+            {
+                throw new ArgumentException("Person cannot be null");
+            }
+
+            return SayHello(person.Name);
+        }
+    }
+}
---
+++ 14_validatingParameters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_validatingParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mariel");
+            Console.WriteLine(person.SayHello(string.Empty));
+
+            Console.ReadKey();
+        }
+    }
+}
